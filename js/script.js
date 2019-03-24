4805b91ca20c3554bfc3548ffdabc63ce89bfc85
Damien Holden <damien.holden91@gmail.com>
2019-03-24 21:53:57 +0000

    Final files for CT tech assessment

--- js/script.js
+++ js/script.js
@@ -1,20 +1,74 @@
-window.onload = function(e){ 
-    document.getElementById('contentContainer').setAttribute('class', 'container fadeIn animated');
-//Start setting things up
-getJSONFile();
 
-//Add tooltips
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  })
+function indexSetup(){
+    //Start setting things up
+    getJSONFile();
+    //Add event listeners
+    sortbyFilter();
+    //enable tooltips
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip('enable');
+    });
+    $('#contentContainer').addClass('fade-in');
+    $('#contentContainer').css('display', 'block');
+}
 
-  //Add event listeners
-sortbyFilter();
+function detailsSetup(){
+    //Get the selected car info
+    var info = JSON.parse(sessionStorage.getItem("carInfo"));
+   // buildBlock(info);
+   createSingleCarBlock(info);
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip('enable');
+    });
+    $('#contentContainer').addClass('fadeIn animate');
+    $('#contentContainer').css('display', 'block');
+    $('#singleCar').addClass('fade-in animate');
+    
+}
 
+function createSingleCarBlock(carDetails){
 
-};
+    var individualCar =  `
+    <div class="col-lg-12 col-md-12 col-sm-12">
+        <div class="card">
+            <div class="row">
+                <div class="col-lg-3 col-md-4 col-sm-12">  
+                    <h5 class="car-title font-weight-bold">${carDetails.Vehicle.VehMakeModel['@Name']}</h5>
+                    <img src="${carDetails.Vehicle.PictureURL}" alt="${carDetails.Vehicle.VehMakeModel['@Name']}" class="card-img-top">
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12 text-center">
+                            <i class="fas fa-car-side" data-toggle="tooltip" data-placement="top" title="" data-original-title="Doors: ${carDetails.Vehicle["@DoorCount"]}"></i> : ${carDetails.Vehicle["@DoorCount"]} | 
+                            <i class="fas fa-suitcase-rolling" data-toggle="tooltip" data-placement="top" title="" data-original-title="Bags: ${carDetails.Vehicle["@BaggageQuantity"]}"></i> : ${carDetails.Vehicle["@BaggageQuantity"]} | 
+                            <i class="fas fa-male" data-toggle="tooltip" data-placement="top" title="" data-original-title="Passengers: ${carDetails.Vehicle["@PassengerQuantity"]}"></i> : ${carDetails.Vehicle["@PassengerQuantity"]}
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-8 col-md-8 col-sm-12">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-lg-6 col-md-6 col-sm-12">
+                                <h5>Features</h5>
+                                    <ul class="list-group">
+                                        <li class="list-group-item"><i class="fas fa-snowflake" data-toggle="tooltip" data-placement="top" title="" data-original-title="Air Conditioning: ${carDetails.Vehicle["@AirConditionInd"]}"></i> : ${carDetails.Vehicle["@AirConditionInd"]}</li>
+                                        <li class="list-group-item"><i class="fas fa-cog" data-toggle="tooltip" data-placement="top" title="" data-original-title="Transmission: ${carDetails.Vehicle["@TransmissionType"]}"></i> : ${carDetails.Vehicle["@TransmissionType"]}</li>
+                                        <li class="list-group-item"><i class="fas fa-gas-pump" data-toggle="tooltip" data-placement="top" title="" data-original-title="Fuel: ${carDetails.Vehicle["@FuelType"]}"></i> : ${carDetails.Vehicle["@FuelType"]}</li>
+                                        <li class="list-group-item">Drive Type: ${carDetails.Vehicle["@DriveType"]}</li>
+                                    </ul>
+                                    
+                            </div>
+                            <div class="col-lg-6 col-md-6 col-sm-12">
+                            <label>Total Amount:</label> <br/>
+                            <h4 class="font-weight-bold"> ${new Intl.NumberFormat('ir-IR', { style: 'currency', currency: carDetails.TotalCharge["@CurrencyCode"] }).format(carDetails.TotalCharge["@RateTotalAmount"])}</h4> <br/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>`;
 
-
+    document.getElementById("singleCar").innerHTML += individualCar;
+}
 
 function getJSONFile(){
     var requestURL = 'http://www.cartrawler.com/ctabe/cars.json';
@@ -29,18 +83,19 @@ function getJSONFile(){
                 console.log("Could not get information at this time");
                 $('#errorBanner').addClass('slideInUp animate');
                 $('#errorBanner').css('display', 'block');
-                errorBanner
             }
             else{
             //Default is sorted Ascending
-            //check the filters
+            //check the current active filter setting
             if(document.getElementById("filterBy").value == "priceAsc")
                 var orderedArray = orderbyPriceAsc(jsonResponse);
                 else
                 var orderedArray = orderbyPriceDesc(jsonResponse);
+                //Create Car Blocks
                 createCarBlocks(jsonResponse,orderedArray);
             }
         }
+       
     }
     catch{
         $('#errorBanner').addClass('slideInUp animate');
@@ -59,31 +114,18 @@ function orderbyPriceAsc(jsonResponse){
             cars.push(vehiclesAvail[k]);
         }
     }
-    //sort the array
+    //sort the array by price
     cars.sort(function(a, b){return a.TotalCharge["@RateTotalAmount"]-b.TotalCharge["@RateTotalAmount"]});
-    //console.log(cars);
     return cars;
 }
 
 function orderbyPriceDesc(jsonResponse){
-    var vendors = jsonResponse[0].VehAvailRSCore['VehVendorAvails'];
-    var cars= [];
-    for (var i = 0; i < vendors.length; i++) { 
-        //go through the vendors
-        var vehiclesAvail = vendors[i].VehAvails;
-        for (var k = 0; k < vehiclesAvail.length; k++){
-            //add all cards to array
-            cars.push(vehiclesAvail[k]);
-        }
-    }
-    //sort the array
-    cars.sort(function(a, b){return a.TotalCharge["@RateTotalAmount"]- b.TotalCharge["@RateTotalAmount"]});
-    //console.log(cars);
-    
-    return cars.reverse();
+    var carsAsc = orderbyPriceAsc(jsonResponse);
+    return carsAsc.reverse();
 }
 
 function createCarBlocks(jsonObj, cars) {
+    //Clear the cars block and request the data again and process incase of changes
     document.getElementById("cars").innerHTML = "";
     var vendors = jsonObj[0].VehAvailRSCore['VehVendorAvails'];
     var searchCredentials = jsonObj[0].VehAvailRSCore['VehRentalCore'];
@@ -104,6 +146,7 @@ function createCarBlocks(jsonObj, cars) {
     document.getElementById("resultsCount").innerHTML = cars.length;
 
       for (var j = 0; j < cars.length; j++) {
+        //create an individual car block
         var individualCar =  `
         <div class="col-lg-12 col-md-12 col-sm-12">
             <div class="card">
@@ -135,7 +178,7 @@ function createCarBlocks(jsonObj, cars) {
                                 <div class="col-lg-6 col-md-6 col-sm-12">
                                 <label>Total Amount:</label> <br/>
                                 <h4 class="font-weight-bold"> ${new Intl.NumberFormat('ir-IR', { style: 'currency', currency: cars[j].TotalCharge["@CurrencyCode"] }).format(cars[j].TotalCharge["@RateTotalAmount"])}</h4> <br/>
-                                <a href="#" onclick="moreInfo()" class="btn btn-primary">More Information</a>
+                                <a href="" onclick="moreInfo('${cars[j].Vehicle["@Code"]}')" class="btn btn-primary">More Information</a>
                                 </div>
                             </div>
                             
@@ -144,7 +187,7 @@ function createCarBlocks(jsonObj, cars) {
                 </div>
             </div>
         </div>`;
-
+        //Add car to the existing group
         document.getElementById("cars").innerHTML += individualCar;
       }
   }
@@ -152,21 +195,45 @@ function createCarBlocks(jsonObj, cars) {
 function sortbyFilter(){
     var filterDropdown = document.getElementById("filterBy");
     filterDropdown.addEventListener("change", function(){
-        if (filterDropdown.value == "priceDesc"){
         getJSONFile();
-        document.getElementById('cars').setAttribute('class', 'row fadeIn animated');    
-        }
-        else{
-        getJSONFile();
-        document.getElementById('cars').setAttribute('class', 'row fadeIn animated');
-        }    
+        document.getElementById('cars').setAttribute('class', 'row fade-in');   
         setTimeout(function(){
-            $('#cars').removeClass('fadeIn animated');
-    }, 1000);
+            $('#cars').removeClass('fade-in');
+    }, 1000); 
+         
     })
 }
 
-function moreInfo(){
-    window.location = 'details.html';
+function moreInfo(carCode){
+
+    //fetch the cars list
+    var filterDropdownOrder = document.getElementById("filterBy").value;
+    var orderedArray;
+    var requestURL = 'http://www.cartrawler.com/ctabe/cars.json';
+    var request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function() {
+        var jsonResponse = request.response;
+        var vendors = jsonResponse[0].VehAvailRSCore['VehVendorAvails'];
+
+    //sort array based on how the user left it
+    if (filterDropdownOrder == "priceDesc")
+    orderedArray = orderbyPriceDesc(jsonResponse);
+    else
+    orderedArray = orderbyPriceAsc(jsonResponse);
+    
+    //Find the car using its code
+    for (var i = 0; i < orderedArray.length; i++) { 
+           if (orderedArray[i].Vehicle["@Code"] == carCode){
+            console.log(orderedArray[i]);
+            sessionStorage.setItem("carInfo", JSON.stringify(orderedArray[i]));
+            window.location = 'details.html';
+            break;
+            }
+
+        }
+    }       
 }
 
